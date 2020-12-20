a7e63c78296331621aa3c104e246db06d963259f
Alice Reuter <alice@alicereuter.com>
2020-12-20 16:19:16 -0600

    worked more on day 7 parser

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -3,9 +3,15 @@ use std::io::{self,BufRead};
 use std::path::Path;
 use std::collections::HashMap;
 
+#[derive(Debug)]
 struct Bag {
     can: Vec<String>,
     name: String
+}
+
+struct Contains {
+    name: String,
+    num: i32,
 }
 
 
@@ -15,6 +21,7 @@ fn main() {
         for line in lines {
             if let Ok(rule) = line {
                 let bag = parse_rule(&rule);
+                println!("{:?}",bag);
                 let insname = bag.name.clone();
                 bags.insert(insname,bag);
             }
@@ -25,14 +32,27 @@ fn main() {
 fn parse_rule (rule: &str) -> Bag {
     let parts = rule.split("contain").collect::<Vec<&str>>();
     let part2 = parts[1].split(",").collect::<Vec<&str>>();
-    let mut contains = Vec::new();
-    contains.push(*part2[0]);
-    contains.push(*part2[1]);
-
+    let mut contains : Vec<String> = Vec::new();
+    if part2.len() == 2 {
+        contains.push(part2[0].to_string());
+        let mut s1 = part2[1].to_string();
+        s1.pop();
+        contains.push(s1);
+    } else {
+        let mut s1 = part2[0].to_string();
+        s1.pop();
+        contains.push(s1);
+    }
     Bag {
         can: contains,
-        name: "hi".to_string()
+        name: parts[0].to_string()
     }
+}
+
+fn parse_bag_num (mut bag: &str) {
+    bag.remove(0);
+    bag.split_once(" ");
+
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
