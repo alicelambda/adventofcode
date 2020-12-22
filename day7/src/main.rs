d2b8ac73624c313d1bad409fec64fffb88df123e
Alice Reuter <alice@alicereuter.com>
2020-12-22 13:42:43 -0600

    found bug in day 7 parser

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -28,20 +28,57 @@ fn main() {
             }
         }
     }
-    println!("{:?}",bags);
+    for (key,val) in &bags {
+        println!("bags: {} {:?}",key,val);
+    }
+    println!("{}",traverse(&bags));
+}
+
+fn traverse (bags: &HashMap<String,Bag>) -> i64{
+    let mut cancontain = 0;
+    for (bag, _) in bags {
+        if bag != "shiny gold" {
+            cancontain += traverse_help(bag, bags);
+        }
+    }
+    cancontain
+}
+
+fn traverse_help(curbag: &str, bags: &HashMap<String,Bag>) -> i64{
+    println!("{:?}",curbag);
+    let  con= bags.get(curbag).unwrap();
+    for i in &con.can {
+        if i.name == "shiny gold" {
+            return 1
+        } else {
+            traverse_help(&i.name, bags);
+        }
+    }
+    0
 }
 
 fn parse_rule (rule: &str) -> Bag {
     let parts = rule.split("contain").collect::<Vec<&str>>();
     let part2 = parts[1].split(",").collect::<Vec<&str>>();
     let mut contains : Vec<Contains> = Vec::new();
+    let mut name = parts[0].to_string();
+    name.pop();
+    name.pop();
     if part2.len() == 2 {
-        match  parse_bag_num(&part2[0].to_string()) {
+        let mut s1 = part2[0].to_string();
+        s1.pop();
+        println!("1 {}",s1);
+        match  parse_bag_num(&s1) {
             Some(rule) => contains.push(rule),
             None => {}
         }
         
         let mut s1 = part2[1].to_string();
+        s1.pop();
+        println!("2 {}", s1);
+        println!("1 {}",name);
+        println!("1 {}",name);
+        println!("1 {}",name);
         s1.pop();
         match parse_bag_num(&s1) {
             Some(rule) => contains.push(rule),
@@ -50,14 +87,13 @@ fn parse_rule (rule: &str) -> Bag {
     } else {
         let mut s1 = part2[0].to_string();
         s1.pop();
+        println!("3 {}", s1);
         match parse_bag_num(&s1) {
             Some(rule) => contains.push(rule),
             None => {}
         }
 
     }
-    let mut name = parts[0].to_string();
-    name.pop();
     Bag {
         can: contains,
         name: name
